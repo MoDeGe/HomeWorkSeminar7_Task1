a1549a5f020a4a49ec5f08a461468f41266cd657
MoDeGe <md57@ya.ru>
2022-09-01 17:31:49 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double [,] GetArray(int rows, int columns, int min, int max)
+{
+    double [,] result = new double [rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            double realNumber;
+            Random rnd = new Random();
+            realNumber = rnd.NextDouble();
+            result[i,j] = new Random().Next(min, max) + realNumber;
+            result[i,j] = Math.Round(result[i,j], 2);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double [,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
